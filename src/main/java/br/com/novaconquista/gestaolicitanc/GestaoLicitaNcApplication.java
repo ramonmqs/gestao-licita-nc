84ebb3f7f58079007f788f8721f2c142db27a0fd
ramonmqs <ramonlmarques@hotmail.com>
2026-08-10 09:59:41 -0300

    chore: inicializa projeto spring boot gestao licita nc com dependencias base

---
+++ src/main/java/br/com/novaconquista/gestaolicitanc/GestaoLicitaNcApplication.java
@@ -0,0 +1,13 @@
+package br.com.novaconquista.gestaolicitanc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoLicitaNcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoLicitaNcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/novaconquista/gestaolicitanc/GestaoLicitaNcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.novaconquista.gestaolicitanc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoLicitaNcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
